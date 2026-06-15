96302b0df799e7b21ec3e51d6f2a438bb2525922
Roberta Soares <roberthasss@hotmail.com>
2026-06-14 21:56:25 -0300

    feat: melhorias no app

---
+++ app/src/main/java/com/example/horario/MeuApp.java
@@ -0,0 +1,22 @@
+package com.example.horario;
+
+import android.app.Application;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+public class MeuApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+
+        db.setFirestoreSettings(settings);
+    }
+}
